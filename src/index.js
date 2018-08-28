64965e4e417af1745e66ca6ab89d3317b8a8e8d0
Yassir <hartaniyassir@gmail.com>
2018-08-28 10:02:30 +0100

    remove the default ReadMe

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+  class Square extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {
+        value: null,
+      };
+    }
     render() {
       return (
-        <button className="square">
+        <button className="square" onClick={ () => alert('click') }>
           {this.props.value}
         </button>
       );
