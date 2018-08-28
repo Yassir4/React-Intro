46913b6a2969f54f4013b3876993ca7dd6fdad2c
Yassir <hartaniyassir@gmail.com>
2018-08-28 10:52:07 +0100

    mark an X when a square is clicked

--- src/index.js
+++ src/index.js
@@ -11,8 +11,11 @@ import './index.css';
     }
     render() {
       return (
-        <button className="square" onClick={ () => alert('click') }>
-          {this.props.value}
+        <button
+          className="square"
+          onClick={ () => this.setState({value: 'X'}) }
+        >
+          {this.state.value}
         </button>
       );
     }
